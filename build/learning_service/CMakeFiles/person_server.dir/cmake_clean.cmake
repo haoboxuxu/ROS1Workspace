7aa43d5c42e047f8e169a8c6d271192bd228af2b
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-03-31 20:13:42 +0800

    update

---
+++ build/learning_service/CMakeFiles/person_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/person_server.dir/src/person_server.cpp.o"
+  "/home/haoboxuxu/my_wordspace/devel/lib/learning_service/person_server.pdb"
+  "/home/haoboxuxu/my_wordspace/devel/lib/learning_service/person_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/person_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
