6a88289d7c5be16a0388364e4b738226d87255eb
haoboxuxu <32588400+haoboxuxu@users.noreply.github.com>
2020-04-09 15:57:08 +0800

    update

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,9 +131,11 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/haoboxuxu/my_wordspace/build/gtest/cmake_install.cmake")
+  include("/home/haoboxuxu/my_wordspace/build/learning_launch/cmake_install.cmake")
   include("/home/haoboxuxu/my_wordspace/build/learning_parameter/cmake_install.cmake")
   include("/home/haoboxuxu/my_wordspace/build/learntopic/cmake_install.cmake")
   include("/home/haoboxuxu/my_wordspace/build/learning_service/cmake_install.cmake")
+  include("/home/haoboxuxu/my_wordspace/build/learning_tf/cmake_install.cmake")
 
 endif()
 
